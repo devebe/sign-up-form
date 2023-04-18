2663d81803ba97fff996430f15a48b2233bdd9c0
devebe <dvbirdja@gmail.com>
2023-04-18 21:16:35 +0200

    dingen

--- JS/password-match.js
+++ JS/password-match.js
@@ -1,3 +1,10 @@
-function passwordMatch {
-    const password1 = document.querySelector()
-}
+function passwordMatch() {
+    const passwordFirst = document.getElementById("password1").value;
+    const passwordSecond = document.getElementById("password2").value;
+    
+    if ((passwordFirst != null) && (passwordSecond != null)) {
+        console.log(passwordFirst);
+    }
+};
+
+passwordMatch();

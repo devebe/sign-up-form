9c6f142361b7a55271bafe4fc2023bfa004f444a
devebe <dvbirdja@gmail.com>
2023-04-20 01:27:18 +0200

    A lot of additions to password matching validation

--- JS/password-match.js
+++ JS/password-match.js
@@ -1,24 +1,43 @@
-
-const password1 = document.getElementById("password1").value;
-const password2 = document.getElementById("password2").value;
-
-const passwordMatch = (password1, password2) => {
-    if ((password1 != null) || (password2 != null)) {
-        if (password1 == password2) {
-            const successMessage = document.createElement("p");
-            const successText = document.createTextNode("Password matches");
-            successMessage.classList.add("success");
-            successMessage.appendChild(successText);
-            const element = document.querySelector("password-1");
-            element.appendChild(successMessage);
-            return;
+function passwordMatch() {
+    const a = document.getElementById('password1').value;
+    const b = document.getElementById('password2').value;    
+    if ((a == b) && ((a != '') || (b != ''))) {
+        const successMessage = document.createElement("p");
+        const successText = document.createTextNode("Password matches");
+        successMessage.classList.add("success");
+        successMessage.appendChild(successText);
+        const success = document.getElementById('password-1');
+        success.appendChild(successMessage);
+    }
+    else {
+        const errorMessage = document.createElement("p");
+        const errorText = document.createTextNode("Passwords do not match");
+        errorMessage.classList.add("error");
+        errorMessage.appendChild(errorText);
+        const error = document.getElementById('password-1');
+        error.appendChild(errorMessage);
+        const borders = document.getElementsByClassName('passwordfield');
+        for (const border of borders) {
+            border.classList.add("error-border");
         }
-        else {
-            const errorMessage = document.createElement("p");
-            const errorText = document.createTextNode("Passwords do not match");
-
-        }
-    };
+    }   
 };
 
-passwordMatch(password1,password2);
+document.getElementById('formSubmit').addEventListener('click',passwordMatch);
+
+function removeHelperMessage() {
+    if (document.querySelector('p.success') != null){
+        element = document.querySelector('p.success');
+    }
+    if (document.querySelector('p.error') != null){
+        element = document.querySelector('p.error');
+        const borders = document.getElementsByClassName('passwordfield');
+        for (const border of borders) {
+            border.classList.remove("error-border");
+        }
+    }
+    element.remove();
+};
+
+document.getElementById('password1').addEventListener('click',removeHelperMessage);
+

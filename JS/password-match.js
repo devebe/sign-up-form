a4d85cb6bd7cdab1225b009d1c09e175dbbae1cd
devebe <dvbirdja@gmail.com>
2023-04-19 23:19:09 +0200

    hallo

--- JS/password-match.js
+++ JS/password-match.js
@@ -1,31 +1,24 @@
-//compare values in password1 and password2
+
 const password1 = document.getElementById("password1").value;
 const password2 = document.getElementById("password2").value;
 
 const passwordMatch = (password1, password2) => {
     if ((password1 != null) || (password2 != null)) {
-        if password1 == password2 {
+        if (password1 == password2) {
             const successMessage = document.createElement("p");
             const successText = document.createTextNode("Password matches");
             successMessage.classList.add("success");
             successMessage.appendChild(successText);
-            const element = document.querySelector("")
+            const element = document.querySelector("password-1");
+            element.appendChild(successMessage);
+            return;
         }
-    }
-    //if one or both of the values is empty, do not show any changes to the UI
-    //compare values in password1 and password2
-    //if values are not matching, show error color border and error text under first input
-    //if values are matching, show success text under first input
-}
+        else {
+            const errorMessage = document.createElement("p");
+            const errorText = document.createTextNode("Passwords do not match");
 
-
-function passwordMatching() {
-    const passwordFirst = document.getElementById("password1").value;
-    const passwordSecond = document.getElementById("password2").value;
-    
-    if ((passwordFirst != null) && (passwordSecond != null)) {
-        console.log(passwordFirst);
-    }
+        }
+    };
 };
 
-const formSubmit = document.getElementById("formSubmit").addEventListener("click")
+passwordMatch(password1,password2);
